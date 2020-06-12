89c6c26b5a5057ae0ea4d0c4dc615ef288965aae
pratikhire95@gmail.com <pratikhire95@gmail.com>
2020-06-12 13:06:13 +0530

    Updates

---
+++ precache-manifest.9e656c9cbefeb9c260f1ccd006366587.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3df3332472beae274a4a6337e74ba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cb2ffdfa8bb5cd207030",
+    "url": "/static/css/main.e1816337.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb2ffdfa8bb5cd207030",
+    "url": "/static/js/main.51d21d26.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
